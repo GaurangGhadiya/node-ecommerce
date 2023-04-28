fec70f77e016c83461be6c663432b620d167aed8
gaurang007 <gaurangghadiya99@gmail.com>
2023-04-28 18:39:12 +0530

    add cors

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ const app = express();
 // Add cors
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 // parse body data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
